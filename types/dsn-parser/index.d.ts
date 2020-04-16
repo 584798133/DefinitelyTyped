6e60cddb5cab1d6dd36d63c64e6d421faf746547
wb-sjt634942 <wb-sjt634942@alibaba-inc.com>
2020-04-16 15:12:55 +0800

    add comment

--- types/dsn-parser/index.d.ts
+++ types/dsn-parser/index.d.ts
@@ -1,3 +1,9 @@
+// Type definitions for dsn-parser
+// Project: https://github.com/fonini/dsn-parser
+// Definitions by: SuJiangTao <https://github.com/584798133>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.6
+
 declare module 'dsn-parser' {
     interface IParts {
         driver: string;
